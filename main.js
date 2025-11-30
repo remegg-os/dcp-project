c22889c43c302fd52a4532d33450de47b70663d0
Rahmadi MG <rahmadi.ghani@gmail.com>
2025-11-30 17:55:32 +0700

    Update JS ke ES6 Modern

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 const tombolMenu = document.querySelector('.menu-toggle');
 const menuNav = document.querySelector('.nav-links');
 const menuLinks = document.querySelectorAll('.nav-links a');
-tombolMenu.addEventListener('click', function() {
+tombolMenu.addEventListener('click', () => {
     menuNav.classList.toggle('active');
 });
 menuLinks.forEach(link => {
